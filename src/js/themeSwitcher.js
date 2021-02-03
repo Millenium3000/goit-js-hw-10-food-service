e51928ce89e4d1c4f6f9e8af494ed392d9411f40
Maxim Shulgin <66272852+Millenium3000@users.noreply.github.com>
2021-02-03 22:09:08 +0200

    second  commit

--- src/js/themeSwitcher.js
+++ src/js/themeSwitcher.js
@@ -1,21 +1,29 @@
-const body = document.querySelector('body');
 const switcher = document.querySelector('.theme-switch__toggle');
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-switcher.addEventListener('click', setTheme);
+switcher.addEventListener('change', setTheme);
 switcher.checked = false;
 
 function setTheme() {
-  if (this.checked) {
-    body.classList.remove(Theme.LIGHT);
-    body.classList.add(Theme.DARK);
+  const bodyStyle = document.body.classList.contains(Theme.DARK);
+  if (!bodyStyle) {
+    document.body.classList.remove(Theme.LIGHT);
+    document.body.classList.add(Theme.DARK);
     localStorage.setItem('theme', 'dark-theme');
   } else {
-    body.classList.remove(Theme.DARK);
-    body.classList.add(Theme.LIGHT);
-    localStorage.setItem('theme', 'light-theme');
+    document.body.classList.remove(Theme.DARK);
+    document.body.classList.add(Theme.LIGHT);
+    localStorage.removeItem('theme', 'dark-theme');
+  }
+}
+setThemeLocal();
+function setThemeLocal() {
+  const theme = localStorage.getItem('theme');
+  if (theme) {
+    setTheme();
+    switcher.checked = true;
   }
 }
